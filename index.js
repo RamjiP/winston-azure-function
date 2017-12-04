0de5033ad2720a03a7811f28bcf3def2cc5745c0
Ramji <ramji.p@outlook.com>
2017-12-04 11:08:06 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var Transport = require('winston-transport');
+
+module.exports = class WinstonAzureFunction extends Transport {
+
+
+    constructor(opts) {
+        super(opts);
+
+        this.name = 'azure-functions';
+        this.context = options.context;
+        this.level = options.level || 'info';
+
+        return this;
+    }
+
+    log(info, callback) {
+        setImmediate(function () {
+            this.emit('logged', info);
+        });
+
+        // Perform the writing to the remote service
+        if (this.context.log[level]) {
+            this.context.log[level]('[' + level + '] ' + message);
+        } else {
+            this.context.log('[' + level + '] ' + message);
+        }
+        callback();
+    }
+}
